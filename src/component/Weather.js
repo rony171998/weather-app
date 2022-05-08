5cbe53c2251217d7f91f2843c5206f00aab0dc3a
rony171998 <rony171998@gmail.com>
2022-05-08 10:31:35 -0500

    ChangeCity

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -1,35 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import axio from "axios"
 import "./Weather.css"
+import NewCity from './NewCity';
 
 const Weather = () => {
-
 
     useEffect(() => {
         function success(pos) {
             var crd = pos.coords;
             axio.get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=1581b57091b320b7a370afc58058cb59`)
-                .then((res) => setWeather(res.data));
+            .then((res) => setWeather(res.data));
 
+            const lats =[40.81667,40.4165,35.6895,61.21806]
+            const lons =[140.73333,-3.70256,139.69171,-149.90028]
 
-            const latEspaña = 40.4165
-            const lonEspaña = -3.70256
-            axio.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latEspaña}&lon=${lonEspaña}&appid=1581b57091b320b7a370afc58058cb59`)
-                .then((res) => setWeatherx(res.data));
+            let ramdonlatlon=Math.floor(Math.random() * (4 - 1)) + 1;
+            
+            axio.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lats[ramdonlatlon]}&lon=${lons[ramdonlatlon]}&appid=1581b57091b320b7a370afc58058cb59`)
+            .then((res) => setWeatherx(res.data));
         }
         navigator.geolocation.getCurrentPosition(success);
-
     }, [])
-
-
+    
     const [Weatherx, setWeatherx] = useState({})
     const [Weather, setWeather] = useState({})
     const [signGrades, setSignGrades] = useState("F")
-    const [grades, setGrades] = useState(Weather.main?.temp)
-
+    const [grades, setGrades] = useState(0)
+    
     const ChangeCity = () => {
+        
+        let aux=Weather
         setWeather(Weatherx)
+        setWeatherx(aux)  
     }
+    
     const ChangeSignGrades = () => {
         if (signGrades === "F") {
             setSignGrades("C");
@@ -38,7 +42,7 @@ const Weather = () => {
         }
     }
     const ChangeGrades = () => {
-        const value = Weather.main?.temp
+        const value =(grades===0 ? Weather.main?.temp.toFixed() :grades)
         ChangeSignGrades();
         if (signGrades === "F") {
             setGrades(((value - 32) * 5 / 9).toFixed())
@@ -54,7 +58,9 @@ const Weather = () => {
         , lighyrain: "https://media.nationalgeographic.org/assets/photos/000/263/26383.jpg"
         , moderaterain: "https://media.nationalgeographic.org/assets/photos/000/263/26383.jpg"
         , clear: "https://www.langleyadvancetimes.com/wp-content/uploads/2020/03/20890720_web1_Langley-Weather-Sun-Clear-Sky-Skies.jpg"
-
+        , snowy: "https://www.vmcdn.ca/f/files/via/images/weather/vancouver-weather-forecast-december-2021-snowfall.jpg;w=960;h=533;mode=crop"
+        , snowyflare: "https://www.vmcdn.ca/f/files/via/images/weather/vancouver-weather-forecast-december-2021-snowfall.jpg;w=960;h=533;mode=crop"
+        , fog: "https://ychef.files.bbci.co.uk/976x549/p03j2k8n.jpg"
     }
 
     const ChangeBackground = () => {
@@ -65,12 +71,10 @@ const Weather = () => {
                 index = keyss.length
             }
         }
-
     }
     ChangeBackground();
 
-
-
+    
     return (
         <div className='Weather'>
             <div className='WeatherBox'>
@@ -83,8 +87,8 @@ const Weather = () => {
                     <h2>Weather: {Weather.weather?.[0].main}</h2>
                     <h2>Clouds: {Weather.clouds?.all} %</h2>
                     <h2>Wind Speed: {Weather.wind?.speed} m/s</h2>
-                    <h2>Pressure: {Weather.main?.pressure} mb</h2>
-                    <h2>Temp: {grades} °{signGrades}</h2>
+                    <h2>Pressure: {Weather.main?.pressure} mb</h2>                   
+                    <h2>Temp: {grades===0 ? Weather.main?.temp.toFixed() :grades} °{signGrades}</h2>
                     <button onClick={ChangeGrades}>Degrees</button>
                 </div>
             </div>
